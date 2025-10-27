394de990b48cf3992ff9e90b7a92a35c3e1844f5
regawan <regawan@regawan-S540-ubt-24.04>
2025-10-27 21:47:19 +0800

    Draft of simulator

---
+++ comp4651-25fall-assignment-2/simulator.py
@@ -0,0 +1,42 @@
+def read_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            return lines
+    except FileNotFoundError:
+        print(f"Error: The file at {file_path} not found.")
+    except IOError:
+        print(f"Error: IO error while reading the file at {file_path}.")
+
+def handle_instruction(instruction, acc):
+    match instruction[0]:
+        case "add":
+            print(f"[Guest] Executing: {instruction[0]} {instruction[1]}")
+            acc += instruction[1]
+        case "print":
+            print(f"[Guest] Executing: {instruction[0]}")
+            print(f"Accumulator value: {acc}")
+        case "scan_disk":
+            print("[VMM] Executing: Trapped privileged instruction 'scan_disk', emulating...")
+        case "halt":
+            print("[VMM] Executing: Trapped privileged instruction 'halt'. Halting guest.")
+            break
+        case _:
+            print(f"Unknown instruction: {instruction[0]}")
+
+def main():
+    acc = 0
+
+    file_path = 'guest_program.txt'
+
+    lines = read_file(file_path)
+    if lines:
+        for line in lines:
+            if not line:
+                continue
+            instruction = line.strip().split()
+
+            handle_instruction(instruction, acc)
+
+if __name__ == "__main__":
+    main()
